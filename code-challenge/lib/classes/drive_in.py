26a81ccccd8903fa53fb14d0125335780c34de09
shawn <shawnsmith0024@gmail.com>
2023-03-09 10:10:19 -0500

    Drive_in initilized

--- code-challenge/lib/classes/drive_in.py
+++ code-challenge/lib/classes/drive_in.py
@@ -1,2 +1,14 @@
 class DriveIn:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,name):
+        if type(name) == str and len(name) > 0:
+            self._name = name
+        else:
+            raise Exception("Name should be a string of 1 or more character!")
